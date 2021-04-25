d68b2108a1aed22f06b1d28ab55d10d6e76945c7
놀욘 <63511753+noryonkr@users.noreply.github.com>
2021-04-25 21:11:34 +0900

    Add files via upload

---
+++ src/commands/uptime.js
@@ -0,0 +1,22 @@
+class Command {
+    constructor (client) {
+        this.client = client
+        this.name = 'uptime'
+        this.aliases = ["업타임"]
+        this.category = 'TEST'
+        this.permissions = ['Everyone']
+        this.usage = ''
+        this.description = ''
+        this.requirements = {
+            allowDM: false,
+            inVoice: false,
+            sameVC: false,
+            sameTC: false,
+            isPlaying: false
+        }
+    }
+
+    async run ({ message }) {
+        message.channel.send(`봇 속도는.. ${this.client.ws.ping}`)
+    }
+}
