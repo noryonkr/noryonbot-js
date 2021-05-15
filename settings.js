80d44472cd0cbe1b64245be748a5500e4fbdac70
놀욘 <63511753+noryonkr@users.noreply.github.com>
2021-05-15 21:58:20 +0900

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: 'ODE3MDU5MzU0NzQyMDk1ODky.YED_0g.exzE5Avp9ZNfbUJO0mqfLSJ4yJM',
+    token: '토큰',
     audio: {
         nodes: [
-            { host: 'cloud.ungbeom.ga', port: 5000, auth: 'youshallnotpass', name: 'Container-1' }
+            { host: 'IP or host', port: 5000, auth: 'youshallnotpass', name: 'Container-1' }
         ],
         shoukakuOptions: { moveOnDisconnect: false, restTimeout: 10000, reconnectTries: 10 }
     }
