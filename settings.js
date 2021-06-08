393fbd4848efa1375db55830ba1564ae4665131f
놀욘 <63511753+noryonkr@users.noreply.github.com>
2021-06-08 20:11:34 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,11 @@
 module.exports = {
-    token: '토큰',
-    audio: {
-        nodes: [
-            { host: 'IP or host', port: 5000, auth: 'youshallnotpass', name: 'Container-1' }
-        ],
-        shoukakuOptions: { moveOnDisconnect: false, restTimeout: 10000, reconnectTries: 10 }
-    }
+  token: 'token',
+  audio: {
+    nodes: [
+        { host: 'IP or 주소', port: 5000, auth: 'youshallnotpass', name: 'Container-1' }
+           ],
+    shoukakuOptions: { moveOnDisconnect: false, restTimeout: 10000, reconnectTries: 10 }
+    },
+  prefix: 's!',
+  owners: ['616570697875193866']
 }
