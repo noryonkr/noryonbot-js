7c40cf4c364d2b800aee761ade8a60ab141c1797
놀욘 <63511753+noryonkr@users.noreply.github.com>
2021-06-16 22:39:41 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,19 @@
 module.exports = {
-  token: 'token',
-  audio: {
-    nodes: [
-        { host: 'IP or 주소', port: 5000, auth: 'youshallnotpass', name: 'Container-1' }
-           ],
-    shoukakuOptions: { moveOnDisconnect: false, restTimeout: 10000, reconnectTries: 10 }
-    },
+  token: 'bot token',
   prefix: 's!',
-  owners: ['616570697875193866']
+  owners: ['ID'],
+  color: '#63aaf7',
+  hook:[{
+    token:'TOKEN',
+    id:'ID'
+  }],
+      emojis: {
+        off: ':x:',
+        error: '⚠',
+        queue: '📋',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+      filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+
 }
